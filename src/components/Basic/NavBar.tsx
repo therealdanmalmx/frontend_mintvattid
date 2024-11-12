823794b5ac649772025166ecd911eb2add09d389
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-11-12 15:29:22 +0100

    Change to Swedish

--- src/components/Basic/NavBar.tsx
+++ src/components/Basic/NavBar.tsx
@@ -2,7 +2,9 @@ const NavBar = () => {
   return (
     <div className="navbar bg-neutral flex justify-between text-slate-200">
       <div>
-        <span className="text-xl">Tid För Tvätt</span>
+        <a href="/" className="text-xl">
+          Tid För Tvätt
+        </a>
       </div>
       <div className="flex-none">
         <div className="dropdown dropdown-end">
@@ -13,20 +15,20 @@ const NavBar = () => {
           >
             <div className="w-10 rounded-full">
               <img
-                alt="Tailwind CSS Navbar component"
+                alt="Inloggad användare"
                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
               />
             </div>
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 text-slate-600 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-max p-2 text-slate-600 shadow"
           >
             <li>
-              <a className="justify-between">Profile</a>
+              <a className="justify-between">Profil</a>
             </li>
             <li>
-              <a>Logout</a>
+              <a>Logga ut</a>
             </li>
           </ul>
         </div>
