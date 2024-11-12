6599b0f555c4e4fd371c63fa8387a01d8c98197a
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-11-12 15:51:22 +0100

    Mock login and logout

--- src/components/Basic/NavBar.tsx
+++ src/components/Basic/NavBar.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const NavBar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="navbar bg-neutral flex justify-between text-slate-200">
       <div>
@@ -7,31 +10,37 @@ const NavBar = () => {
         </a>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Inloggad användare"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+        {loggedIn ? (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Inloggad användare"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-max p-2 text-slate-600 shadow"
+            >
+              <li>
+                <a className="justify-between">Profil</a>
+              </li>
+              <li>
+                <a onClick={() => setLoggedIn(false)}>Logga ut</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-max p-2 text-slate-600 shadow"
-          >
-            <li>
-              <a className="justify-between">Profil</a>
-            </li>
-            <li>
-              <a>Logga ut</a>
-            </li>
-          </ul>
-        </div>
+        ) : (
+          <a className="cursor-pointer" onClick={() => setLoggedIn(true)}>
+            Logga In
+          </a>
+        )}
       </div>
     </div>
   );
