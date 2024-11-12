82523d677d16a482fe357e4a878641de19d432cb
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-11-12 15:20:54 +0100

    first commit

---
+++ src/components/Basic/NavBar.tsx
@@ -0,0 +1,38 @@
+const NavBar = () => {
+  return (
+    <div className="navbar bg-neutral flex justify-between text-slate-200">
+      <div>
+        <span className="text-xl">Tid För Tvätt</span>
+      </div>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 text-slate-600 shadow"
+          >
+            <li>
+              <a className="justify-between">Profile</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
