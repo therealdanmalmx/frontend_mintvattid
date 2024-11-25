6e7087c9a2d335553634c678cf46cad08c59445b
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-11-25 17:13:23 +0100

    Add logo to landing page

--- src/components/Basic/Landing/LandingPage.tsx
+++ src/components/Basic/Landing/LandingPage.tsx
@@ -5,6 +5,11 @@ export default function LandingPage() {
     <div className="h-dvh overflow-hidden bg-slate-200 bg-opacity-90 bg-laundry-room bg-cover bg-center bg-blend-overlay">
       <div className="container mx-auto flex h-full flex-col justify-between px-4 py-12 text-center md:justify-center">
         <div className="space-y-8">
+          <img
+            src="/src/assets/img/tvattid_logo.webp"
+            alt="Tvättid logotyp"
+            className="mx-auto h-32 md:h-48"
+          />
           <h1 className="mt-12 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:mt-0 md:text-6xl">
             Boka tvättid{" "}
             <span className="block md:inline-block">enkelt och smidigt!</span>
